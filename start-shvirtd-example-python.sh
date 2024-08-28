803201de51a9407022d8c255e0dac1738b2ab4be
borbul2007 <78852894+borbul2007@users.noreply.github.com>
2024-08-28 09:37:00 +0300

    Update start-shvirtd-example-python.sh

--- start-shvirtd-example-python.sh
+++ start-shvirtd-example-python.sh
@@ -1,7 +1,4 @@
 #! /bin/bash
 
-cd /opt
-git clone https://github.com/borbul2007/shvirtd-example-python.git && cd shvirtd-example-python
-docker compose up -d
-docker cp script.sql shvirtd-example-python-db-1:/script.sql
-docker exec -t shvirtd-example-python-db-1 /bin/sh -c 'mysql -uroot -pYtReWq4321 < script.sql'
+git clone https://github.com/borbul2007/shvirtd-example-python.git
+cd /opt/shvirtd-example-python && docker compose up -d
